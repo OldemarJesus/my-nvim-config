a8ea2bc43dd51b0022693b11449e432c11ad842d
Oldemar Jesus <oldemego@gmail.com>
2024-03-15 22:23:12 +0000

    my custom nvim config

---
+++ init.lua
@@ -0,0 +1,20 @@
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set expandtab")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup("plugins", {})
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,57 @@
+return {
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "L3MON4D3/LuaSnip" },
+    { 
+        "hrsh7th/nvim-cmp",
+        config = function()
+            local cmp = require("cmp")
+
+            cmp.setup({
+                snippet = {
+                  expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                  end,
+                },
+                window = {
+                  completion = cmp.config.window.bordered(),
+                  documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                  ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                  ['<C-Space>'] = cmp.mapping.complete(),
+                  ['<C-e>'] = cmp.mapping.abort(),
+                  ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                  { name = 'nvim_lsp' },
+                  { name = 'luasnip' },
+                }, {
+                  { name = 'buffer' },
+                })
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "tsserver" }
+            })
+
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup {
+                capabilities = capabilities
+            }
+            lspconfig.tsserver.setup {
+                capabilities = capabilities
+            }
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+        end
+    },
+}
